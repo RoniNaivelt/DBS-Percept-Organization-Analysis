749e4c3eec52b2b121677dc0f46851da08ad3fe6
RoniNaivelt <158068849+RoniNaivelt@users.noreply.github.com>
2025-06-19 23:23:04 +0300

    Delete Organize directory

--- Orgenize/EditPatientInfo.m
+++
@@ -1,6 +0,0 @@
-function PatientInfo = EditPatientInfo(data, DBS_data)   
-    % initialize
-    Patiant_Initials = [data.PatientInformation.Initial.PatientFirstName(1) data.PatientInformation.Initial.PatientLastName(1)];
-    Patient_name = ['Patient_' Patiant_Initials];
-    PatientInfo = DBS_data.(Patient_name).PatientInfo;
-end
